54512f3e8c68d00c0eafff19356cfd498a4a64d3
Peter Wallmeyer <peter.wallmeyer@web.de>
2026-06-01 22:50:27 +0200

    Fix filename and add helpful link

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@ ssh-keygen -t ed25519 -C "info@peterwlm.de" -f $HOME/.ssh/id_ed25519 -N ""
 echo ""
 echo "Please add this SSH Public Key to your GitHub Account:"
 echo ""
-cat ~/.ssh/ed25519.pub
+cat ~/.ssh/id_ed25519.pub
+echo ""
+echo "https://github.com/settings/ssh/new"
 echo ""
 read -p "Press enter to continue..."
 git clone git@github.com:cookie-tea/server-setup.git
 ./server-setup/setup.sh
 rm -r server-setup
-
